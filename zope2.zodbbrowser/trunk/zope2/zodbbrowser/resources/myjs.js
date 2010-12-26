44dd3c7ba673ab52219d8d2406a97739baecc1da
rallende <rallende@7bbb78bc-28ee-11df-93bc-f3f1cd2bf0c3>
2010-12-26 07:15:57 +0000

    the first panel works!

--- zope2.zodbbrowser/trunk/zope2/zodbbrowser/resources/myjs.js
+++ zope2.zodbbrowser/trunk/zope2/zodbbrowser/resources/myjs.js
@@ -1,6 +1,11 @@
+// getPath builds the path traversing the tree
+var getPath = function(node) {
+    if (node.data.title) { return ( getPath(node.parent) + "/" + node.data.title) ; }
+    else { return "" }
+};
+
   $(function(){
     $("#tree").dynatree({
-      // In real life we would call a URL on the server like this:
           initAjax: {
               url: "/tree",
               data: { mode: "funnyMode" },
@@ -11,6 +16,14 @@
       },
       onDeactivate: function(node) {
         $("#echoActive").text("-");
+      },
+      onLazyRead: function(node){
+              node.appendAjax({
+                url: function() { return getPath(node) + "/tree"; }(),
+                data: {key: node.data.key,
+                       mode: "funnyMode"
+                       }
+              });
       }
     });
   });
--- zope2.zodbbrowser/trunk/zope2/zodbbrowser/tree.py
+++ zope2.zodbbrowser/trunk/zope2/zodbbrowser/tree.py
@@ -8,11 +8,13 @@ class Tree(BrowserView):
     """
 
     def context_tree(self):
-        content_tree  =  [ build_tree(self.context ) ] 
+        content_tree  =  build_tree(self.context, 2, 1) 
+        if type(content_tree) == dict:
+            content_tree  =  [ content_tree ] 
         return json.dumps(content_tree, ensure_ascii= True, indent=4)
 
 """Helpers methods"""
-def build_tree(elem, level = 1024):
+def build_tree(elem, level = 1024, remove_root = 0):
         """Levels represents how deep is the tree
         """
         if level <= 0:
@@ -21,18 +23,27 @@ def build_tree(elem, level = 1024):
 
         lista = elem.objectValues()
         node = {}
-
-        node["title"] = get_id(elem)
-        node["children"] = []
-
-        if len(lista):
-            node["key"] = get_id(elem)
-            node["isFolder"] = True
+        children = []
 
         for i in lista:
             result = (build_tree(i, level))
             if result:
-                node["children"].append(result)
+                children.append(result)
+
+        if remove_root:
+            return children
+        else:
+            node["title"] = get_id(elem)
+            node["children"] = []
+
+            if len(lista):
+                node["key"] = get_id(elem)
+                node["isFolder"] = True
+
+            if len(lista) and not len(node["children"]):
+                node["isLazy"] = True
+
+            node["children"] = children
 
         return node 
 
